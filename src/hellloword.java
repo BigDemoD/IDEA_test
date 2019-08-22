36e8b1e144f87b08d91da6d519997c7f883af3b0
admin <605881641@qq.com>
2019-08-22 15:57:13 +0800

    第一次试验提交

---
+++ src/hellloword.java
@@ -0,0 +1,8 @@
+public class hellloword {
+    public static void main(String[] args){
+        System.out.println("hellloword");
+        System.out.println("helloidea");
+        System.out.println("hellogithub");
+        System.out.println("again updata");
+    }
+}
